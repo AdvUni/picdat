a4eae04a17eebfea9e9735408b6ca86364406f78
marloh <marie.lohbeck@uni-ulm.de>
2018-03-13 15:19:54 +0100

    outsourced calculation of abs_val in xml_container into function

---
+++ asup_mode/util.py
@@ -0,0 +1,45 @@
+"""
+This modules contains functions needed for the asup mode.
+"""
+import datetime
+import logging
+
+__author__ = 'Marie Lohbeck'
+__copyright__ = 'Copyright 2018, Advanced UniByte GmbH'
+
+# license notice:
+#
+# This file is part of PicDat.
+# PicDat is free software: you can redistribute it and/or modify it under the terms of the GNU
+# General Public (at your option) any later version.
+#
+# PicDat is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with PicDat. If not,
+# see <http://www.gnu.org/licenses/>.
+
+
+def get_abs_val(this_val, unixtimestamp, val_buffer, time_buffer, buffer_key):
+    """
+    As it seems that the counters storing the values written in the xml data file
+    never get cleared, it is always necessary to calculate: (this_val -
+    last_val)/(this_timestamp - last_timestamp) to get a useful, absolute value.
+    This function does this calculation.
+    :param this_val: the recent 'total' value
+    :param unixtimestamp: the recent time stamp in unixtime format
+    :param val_buffer: The buffer dict which stores the last 'total' value belonging to this_val
+    :param time_buffer: The buffer dict which stores the last timestamp
+    :param buffer_key: The request key which allocates the right places inside the buffers. It
+    refers to the table and instance, the values belong to.
+    """
+    last_val = val_buffer[buffer_key]
+    last_unixtime = time_buffer[buffer_key]
+    abs_val = str((this_val - last_val) / (unixtimestamp - last_unixtime))
+    datetimestamp = datetime.datetime.fromtimestamp(unixtimestamp)
+
+    logging.debug('(recent_val - last_val)/(recent_time - last_time) = (%s - %s)/(%s - %s) = '
+                  '%s (%s)', this_val, last_val, unixtimestamp, last_unixtime, abs_val, buffer_key)
+
+    return abs_val, datetimestamp
